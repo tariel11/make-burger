f5ae5a9a1b9f38d5384c5e7d3c83932d11cdc4f4
Tariel Arslanov <tarielarslanov5@gmail.com>
2023-03-08 23:02:30 +0300

    фуутер изменения

--- src/scenes/Footer.js
+++ src/scenes/Footer.js
@@ -1,24 +1,29 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 import Logo from '../components/Logo'
 import NavList from '../components/NavList'
 
 const Footer = () => {
+  const {pathname} = useLocation();
+
   return (
     <footer>
       <div className='w-2/3 mx-auto py-12 md:py-20'>
         <div className='w-6/6 mx-auto text-center flex flex-col gap-14 text-sm md:text-lg'>
           <Logo/>
-          <NavList
-            state={true}
-          />
-          <div  className='flex justify-center gap-8'>
+
+          {pathname.includes('project') 
+            ? (<></>)
+            : ( <NavList state={true} />)
+          }
+          {/* <div  className='flex justify-center gap-8'>
             <a href="tel:+996(708) 79 11 92">+996(708) 79 11 92</a>
             <a href="email:mtystudio@gmail">@mtystudio@gmail</a>
           </div>
           <div className='flex justify-center gap-5'>
             <a href="https://instagram.com">Инстаграмм</a>
             <a href="https://facebok.com">Фейсбук</a>
-          </div>
+          </div> */}
         </div>
       </div>
     </footer>
